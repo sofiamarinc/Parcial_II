5abf1f3cdab5bb2c7fa93ddb8a18bf89d8deae13
sofiamarinc <sofia.marinc@udea.edu.co>
2023-10-31 19:14:13 -0500

    "Inicio clase marcador"

---
+++ marcador.cpp
@@ -0,0 +1,20 @@
+/*#include "Marcador.h"
+#include <iostream>
+#include "jugador.h"
+Marcador::Marcador() : nombre(""), ficha(' '), puntaje(0) {
+    // Constructor vacío
+}
+
+Marcador::Marcador(string nombre, char ficha) : nombre(nombre), ficha(ficha), puntaje(0) {
+    // Constructor con parámetros
+}
+
+void Marcador::aumentarPuntaje(int puntos) {
+    puntaje += puntos;
+}
+
+void Marcador::mostrarMarcador() {
+    cout << "Jugador: " << nombre << "\nFicha: " << ficha << "\nPuntaje: " << puntaje << std::endl;
+}
+
+*/
---
+++ marcador.h
@@ -0,0 +1,16 @@
+/*#ifndef MARCADOR_H
+#define MARCADOR_H
+
+class Marcador {
+public:
+    string nombre;
+    char ficha;
+    int puntaje;
+
+    Marcador();
+    Marcador(string nombre, char ficha);
+    void aumentarPuntaje(int puntos);
+    void mostrarMarcador();
+};
+
+#endif // MARCADOR_H*/
