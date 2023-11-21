e60613c3f56b81634a63c6b85d4610906e9632fe
sofiamarinc <80049482+sofiamarinc@users.noreply.github.com>
2023-11-21 00:27:24 -0500

    metodo calcularpuntaje
    este metodo se modifica ya que nos parece una mejor manera de manejar el jeugo

--- puntaje.cpp
+++ puntaje.cpp
@@ -1,6 +1,23 @@
-#include "puntaje.h"
+#include "Puntaje.h"
 
-puntaje::puntaje()
-{
+Puntaje::Puntaje() : puntajeJugador1(0), puntajeJugador2(0) {}
 
+void Puntaje::calcularPuntaje(const Tablero& tablero) {
+    //contadores
+    puntajeJugador1 = 0;
+    puntajeJugador2 = 0;
+    /*este cambio es importante ya que vamos a usar las funciones
+     *  nuevas implementadas en tablero para poder iterarlo*/
+    for (int i = 0; i < tablero.getFilas(); ++i) {
+        for (int j = 0; j < tablero.getColumnas(); ++j) {
+            char posicion = tablero.getPosicion(i, j);
+            if (posicion == '-') {   //'-' esta es la ficha siempre del jugador 1
+                ++puntajeJugador1;
+            } else if (posicion == '*') {
+                ++puntajeJugador2;
+            }
+        }
+    }
 }
+
+
