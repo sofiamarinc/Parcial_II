d0bf842ced25460125391c6cf8e20e58d69ea125
sofiamarinc <80049482+sofiamarinc@users.noreply.github.com>
2023-11-22 12:31:21 -0500

    implementacion de guardarresultados
    usamos el mismo codigo que el anterior ahora vemos que al momento de implementarlo no sera de igual forma por lo que esto es un boceto

--- manejotexto.cpp
+++ manejotexto.cpp
@@ -1,6 +1,32 @@
 #include "manejotexto.h"
 
-/*ManejoTexto::ManejoTexto()
-{
+void ManejoTexto::guardarResultados(const string& nombreJugador1, char fichaJugador1, int puntajeJugador1,
+                                    const string& nombreJugador2, char fichaJugador2, int puntajeJugador2,
+                                    const string& ganador) {
+    // Obtener la fecha y hora actual
+    std::time_t now = std::time(nullptr);
+    char* dt = new char[100]; // Reserva memoria para la fecha y hora
+    std::strftime(dt, 100, "%Y-%m-%d %H:%M:%S", std::localtime(&now));
 
-}*/
+    // Abrir archivo en modo append
+    std::ofstream outFile("puntajes.txt", std::ios::app);
+    if (!outFile) {
+        std::cerr << "No se pudo abrir el archivo para escribir los resultados." << std::endl;
+        delete[] dt; // Liberar memoria
+        return;
+    }
+
+    // Escribir los resultados en el archivo
+    outFile << "Jugador 1: " << nombreJugador1
+            << " (Ficha " << fichaJugador1 << ") > Puntaje: " << puntajeJugador1
+            << ", Jugador 2: " << nombreJugador2
+            << " (Ficha " << fichaJugador2 << ") > Puntaje: " << puntajeJugador2
+            << ", Ganador: " << ganador
+            << ", Fecha y hora: " << dt
+            << std::endl;
+
+    // Cerrar el archivo y liberar la memoria
+    outFile.close();
+    delete[] dt;
+}
+
--- manejotexto.h
+++ manejotexto.h
@@ -2,13 +2,15 @@
 #define MANEJOTEXTO_H
 #include <iostream>
 #include <fstream>
+#include <ctime>
 
 using namespace std;
 
 class ManejoTexto {
 public:
-    void revisarHistorial(const string& nombreArchivo);
-    //void guardarresultados()
+    void guardarResultados(const string& nombreJugador1, char fichaJugador1, int puntajeJugador1,
+                           const string& nombreJugador2, char fichaJugador2, int puntajeJugador2,
+                           const string& ganador);
 };
 
 #endif // MANEJOTEXTO_H
